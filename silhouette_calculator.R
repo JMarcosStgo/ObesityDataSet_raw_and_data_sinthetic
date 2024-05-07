d7392bf9e1e639f11f2509ed69c0e7dada3e3b1f
JMarcosStgo <72782781+JMarcosStgo@users.noreply.github.com>
2024-05-06 19:05:52 -0600

    Update silhouette_calculator.R

--- silhouette_calculator.R
+++ silhouette_calculator.R
@@ -20,24 +20,3 @@ fuzzy_silhouette_score <- function(k, dataKM, featureSel) {
   cat("Factor de silueta: ", mean(ss[, 3]), "\n")
   return(mean(ss[, 3]))  # Regresar coeficiente de silueta
 }
-
-
-
-
-
-
-
-
-
-
-
-
-# Función para calcular el coeficiente de silueta
-#silhouette_score <- function(k, dataKM, featureSel) {
-#  cat("Kmeans-Caracteristicas: ", featureSel, "\n")
-#  # Aplicar K-means a los datos dataKM, k-grupos
-#  km <- kmeans(dataKM[, featureSel], centers = k, nstart = 25)
-#  ss <- silhouette(km$cluster, dist(dataKM[, featureSel]))
-# cat("Factor de silueta: ", mean(ss[, 3]), "\n")
-#return(mean(ss[, 3]))  # Regresar coeficiente de silueta
-#}
